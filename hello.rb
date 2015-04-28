e7eab8685eaacce0a63df7019455869bb25f9d18
jkorneg2 <jkorneg2@uncc.edu>
2015-04-28 15:22:04 -0400

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -1,4 +1,5 @@
-# Default is “World”
-name = ARGV.first || “World”
+# Default is World
+# Author: jkorneg2 (jkorneg2@uncc.edu)
+name = ARGV.first || "World"
 
-puts "Hello, #{name}!”
+puts "Hello, #{name}!"
